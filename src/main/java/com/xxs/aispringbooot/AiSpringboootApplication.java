0cdb0c9651ff31e5ad3216bca23207332c19012b
Rosabel <2268311059@qq.com>
2026-07-12 00:26:57 +0800

    脚手架

---
+++ src/main/java/com/xxs/aispringbooot/AiSpringboootApplication.java
@@ -0,0 +1,13 @@
+package com.xxs.aispringbooot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSpringboootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSpringboootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxs/aispringbooot/AiSpringboootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxs.aispringbooot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSpringboootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
