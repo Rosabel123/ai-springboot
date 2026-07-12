854d95a4984692bc50e8f3db112f7ee2ccfccd6d
Rosabel <2268311059@qq.com>
2026-07-12 17:05:44 +0800

    第一天

---
+++ src/main/java/com/xxs/aispringbooot/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.xxs.aispringbooot.common.exception;
+
+import com.xxs.aispringbooot.common.Result;
+import com.xxs.aispringbooot.common.ResultCode;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    //处理参数校验异常
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result<String> handleException(MethodArgumentNotValidException e) {
+        //获取异常信息
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(","));
+        return Result.error(ResultCode.PARAM_ERROR.getCode(), ResultCode.PARAM_ERROR.getMsg(), message);
+    }
+}
+
